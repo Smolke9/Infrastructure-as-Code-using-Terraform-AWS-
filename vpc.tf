623aafba2e5a517b32ca2f693e61065a7b9e6353
Suraj Molke <163307027+Smolke9@users.noreply.github.com>
2025-12-28 22:10:32 +0530

    Create VPC with public subnets and routing

---
+++ vpc.tf
@@ -0,0 +1,32 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "terraform-vpc"
+  }
+}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+resource "aws_subnet" "public" {
+  count = 2
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnets[count.index]
+  availability_zone = element(["ap-south-1a", "ap-south-1b"], count.index)
+  map_public_ip_on_launch = true
+}
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+resource "aws_route_table_association" "public_assoc" {
+  count = 2
+  subnet_id = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
